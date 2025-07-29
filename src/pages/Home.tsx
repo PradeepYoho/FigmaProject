c45ee2ce42b30a40fb1ee11c1d5b91123cfc643f
SelshiyaMaryYoho <selshiyamary.yoho@gmail.com>
2025-07-29 23:55:27 +0530

    Modified files

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import Dashboard from "../components/DashBoard";
 
 const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-center mt-10 px-4">
+    <div className="flex flex-col items-center justify-center mt-10 p-4">
       <div className="w-full max-w-[1280px] min-h-[400px] md:min-h-[600px] lg:min-h-[725px] space-y-6">
      <Dashboard />
       </div>
