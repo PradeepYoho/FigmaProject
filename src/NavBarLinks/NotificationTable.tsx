eb1d3e08c4c0670213f508bb040e8d41114d919c
sathishkumar.yoho <sathishkumar.yoho@gmail.com>
2025-08-01 20:07:24 +0530

    commit changes

--- src/NavBarLinks/NotificationTable.tsx
+++ src/NavBarLinks/NotificationTable.tsx
@@ -32,9 +32,8 @@ const NotificationTable: React.FC = () => {
         </button>
       </div>
 
-   
-     {isOverlayOpen && (
-<div className="absolute top-[-100px] right-0 w-full sm:w-[380px] z-50 p-4">
+   {isOverlayOpen && (
+<div className="absolute top-0 right-0 w-full sm:w-[380px] z-50">
     <div className="bg-white/10 backdrop-blur-xl rounded-xl shadow-lg border border-white/20 p-6 flex flex-col">
       
     
